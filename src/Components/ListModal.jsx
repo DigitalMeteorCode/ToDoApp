18c7b7136c712fa62bc3b05362f104b20f639926
Sarah Beaubeau <sarah.beaubeau@hotmail.fr>
2021-09-21 16:36:06 +0200

    Ajout de Firebase pour la base de données, bug au niveau de l'import de firebase sur Fire.js

--- src/Components/ListModal.jsx
+++ src/Components/ListModal.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
 import { Button, Modal } from 'antd';
 import ListForm from "./ListForm";
+import Fire from "../Fire";
 
 export default function ListModal(props) {
     const [name,setName] = useState("");
     const [color, setColor] = useState("#3B97FF");
+    const [error, setError] = useState(null);
 
     function handleSubmit() {
-        const list = {
-            "name": name,
-            "color": color,
-            "tasks": []
-        }
-        console.log(list);
+        const firebase = new Fire((error) => {
+            if (error) {
+                setError(error);
+            } else {
+                const list = {
+                    "name": name,
+                    "color": color,
+                    "tasks": []
+                }
+                firebase.addList(list);
+            }
+        });
+        props.handleCancel();
     }
 
     return (
         <Modal 
-        title={props.modalTitle} 
-        visible={props.isVisible} 
-        onCancel={props.handleCancel}
-        footer={[
-            <Button type="primary" shape="round" onClick={handleSubmit}>
-                Créer la liste
-            </Button>
-        ]}
+            title={props.modalTitle} 
+            visible={props.isVisible} 
+            onCancel={props.handleCancel}
+            footer={[
+                <Button type="primary" shape="round" onClick={handleSubmit}>
+                    Créer la liste
+                </Button>
+            ]}
         >
-        <ListForm name={name} color={color} setName={setName} setColor={setColor} />
+            {error && <p>Une erreur est survenue</p> }
+            <ListForm name={name} color={color} setName={setName} setColor={setColor} />
       </Modal>
     );
 }
---
+++ src/Fire.js
@@ -0,0 +1,61 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDsuliXndvhklkwH25DDeNINhOaX7RFmKU",
+    authDomain: "todo-list-app-89abd.firebaseapp.com",
+    projectId: "todo-list-app-89abd",
+    storageBucket: "todo-list-app-89abd.appspot.com",
+    messagingSenderId: "731720285104",
+    appId: "1:731720285104:web:969ef93fead20a4d1f3475"
+  };
+
+  export default class Fire {
+    constructor(callback) {
+        if(!firebase.apps.lenght) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                callback(null);
+            } else {
+                firebase.auth().signInAnonymously.catch(error => {
+                    callback(error)
+                });
+            }
+            
+        });
+    }
+    get ref() {
+        return firebase.firestore().collection("lists");//ici le nom lists doit être le même que sur la collection créée sur le site firebase
+    }
+
+    getLists(callback) {
+        let ref = this.ref.orderBy("name");
+        this.unsubscribe = ref.onSnapshot(snapshot => {
+            let lists = [];
+            snapshot.forEach(doc => {
+                lists.push({ id: doc.id, ...doc.data() })
+            });
+            callback(lists);
+        },  function(error) {
+            callback(error);
+        });
+    }
+
+    addList(list) {
+        this.ref.add(list);
+    }
+
+    deleteList(list) {
+        this.ref.doc(list.id).delete();
+    }
+
+    updateList(list) {
+        this.ref.doc(list.id).update(list);
+    }
+
+    detach() {
+        this.unsubscribe();
+    }
+  }
+/*tout ce code compliqué est trouvable dans les documentations de firebase, ce n'est pas le genre de chose que l'on apprend par coeur ! c'est à considérer comme une boîte à outils pour FIREBASE*/
